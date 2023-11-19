ad3a0f925fcc2e137b32ae5059614c864a2587f7
igor <borsbeck@student.21-school.ru>
2023-11-19 21:01:01 +0700

    rename some tables

--- src/part_1.sql
+++ src/part_1.sql
@@ -1,14 +1,15 @@
 -- DROP DATABASE IF EXISTS Retail_Analytics;
 -- CREATE DATABASE Retail_Analytics;
-DROP TABLE IF EXISTS Personal_Information CASCADE;
+DROP TABLE IF EXISTS Customers CASCADE;
 DROP TABLE IF EXISTS Cards CASCADE;
-DROP TABLE IF EXISTS SKU_Groups CASCADE;
-DROP TABLE IF EXISTS Product_Grid CASCADE;
+DROP TABLE IF EXISTS Product_Groups CASCADE;
+DROP TABLE IF EXISTS Products CASCADE;
 DROP TABLE IF EXISTS Stores CASCADE;
 DROP TABLE IF EXISTS Transactions CASCADE;
 DROP TABLE IF EXISTS Checks CASCADE;
--- Create the Personal_Information table
-CREATE TABLE IF NOT EXISTS Personal_Information (
+DROP TABLE IF EXISTS Analysis_Date CASCADE;
+-- Create the Customers table
+CREATE TABLE IF NOT EXISTS Customers (
     Customer_ID SERIAL PRIMARY KEY,
     Customer_Name VARCHAR(50) NOT NULL CHECK (Customer_Name ~ '^[A-Za-zА-Яа-яЁё\s\-]+$'),
     Customer_Surname VARCHAR(50) NOT NULL CHECK (Customer_Surname ~ '^[A-Za-zА-Яа-яЁё\s\-]+$'),
@@ -20,23 +21,23 @@ CREATE TABLE IF NOT EXISTS Personal_Information (
 -- Create the Cards table
 CREATE TABLE IF NOT EXISTS Cards (
     Customer_Card_ID SERIAL PRIMARY KEY,
-    Customer_ID INT REFERENCES Personal_Information(Customer_ID) NOT NULL
+    Customer_ID INT REFERENCES Customers(Customer_ID) NOT NULL
 );
--- Create the SKU_Groups table
-CREATE TABLE IF NOT EXISTS SKU_Groups (
+-- Create the Product_Groups table
+CREATE TABLE IF NOT EXISTS Product_Groups (
     Group_ID SERIAL PRIMARY KEY,
     Group_Name VARCHAR(50) NOT NULL
 );
--- Create the Product_Grid table
-CREATE TABLE IF NOT EXISTS Product_Grid (
+-- Create the Products table
+CREATE TABLE IF NOT EXISTS Products (
     SKU_ID SERIAL PRIMARY KEY,
     SKU_Name VARCHAR(50) NOT NULL,
-    Group_ID INT REFERENCES SKU_Groups(Group_ID) NOT NULL
+    Group_ID INT REFERENCES Product_Groups(Group_ID) NOT NULL
 );
 -- Create the Stores table
 CREATE TABLE IF NOT EXISTS Stores (
     Transaction_Store_ID SERIAL PRIMARY KEY,
-    SKU_ID SERIAL REFERENCES Product_Grid(SKU_ID) NOT NULL,
+    SKU_ID SERIAL REFERENCES Products(SKU_ID) NOT NULL,
     SKU_Purchase_Price NUMERIC(10, 2) NOT NULL,
     SKU_Retail_Price NUMERIC(10, 2) NOT NULL
 );
@@ -51,7 +52,7 @@ CREATE TABLE IF NOT EXISTS Transactions (
 -- Create the Checks table
 CREATE TABLE IF NOT EXISTS Checks (
     Transaction_ID INT REFERENCES Transactions(Transaction_ID) PRIMARY KEY,
-    SKU_ID INT REFERENCES Product_Grid(SKU_ID) NOT NULL,
+    SKU_ID INT REFERENCES Products(SKU_ID) NOT NULL,
     SKU_Amount NUMERIC(10, 2) NOT NULL,
     SKU_Summ NUMERIC(10, 2) NOT NULL,
     SKU_Summ_Paid NUMERIC(10, 2) NOT NULL,
