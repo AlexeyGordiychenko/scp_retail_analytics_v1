33b1589599a78aae69819dda98c7288765266126
igor <borsbeck@student.21-school.ru>
2023-11-19 20:55:58 +0700

    tables creation

---
+++ src/part_1.sql
@@ -0,0 +1,61 @@
+-- DROP DATABASE IF EXISTS Retail_Analytics;
+-- CREATE DATABASE Retail_Analytics;
+DROP TABLE IF EXISTS Personal_Information CASCADE;
+DROP TABLE IF EXISTS Cards CASCADE;
+DROP TABLE IF EXISTS SKU_Groups CASCADE;
+DROP TABLE IF EXISTS Product_Grid CASCADE;
+DROP TABLE IF EXISTS Stores CASCADE;
+DROP TABLE IF EXISTS Transactions CASCADE;
+DROP TABLE IF EXISTS Checks CASCADE;
+-- Create the Personal_Information table
+CREATE TABLE IF NOT EXISTS Personal_Information (
+    Customer_ID SERIAL PRIMARY KEY,
+    Customer_Name VARCHAR(50) NOT NULL CHECK (Customer_Name ~ '^[A-Za-zА-Яа-яЁё\s\-]+$'),
+    Customer_Surname VARCHAR(50) NOT NULL CHECK (Customer_Surname ~ '^[A-Za-zА-Яа-яЁё\s\-]+$'),
+    Customer_Primary_Email VARCHAR(50) UNIQUE CHECK (
+        Customer_Primary_Email ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$'
+    ),
+    Customer_Primary_Phone VARCHAR(15) UNIQUE CHECK (Customer_Primary_Phone ~ '^\+7[0-9]{10}$')
+);
+-- Create the Cards table
+CREATE TABLE IF NOT EXISTS Cards (
+    Customer_Card_ID SERIAL PRIMARY KEY,
+    Customer_ID INT REFERENCES Personal_Information(Customer_ID) NOT NULL
+);
+-- Create the SKU_Groups table
+CREATE TABLE IF NOT EXISTS SKU_Groups (
+    Group_ID SERIAL PRIMARY KEY,
+    Group_Name VARCHAR(50) NOT NULL
+);
+-- Create the Product_Grid table
+CREATE TABLE IF NOT EXISTS Product_Grid (
+    SKU_ID SERIAL PRIMARY KEY,
+    SKU_Name VARCHAR(50) NOT NULL,
+    Group_ID INT REFERENCES SKU_Groups(Group_ID) NOT NULL
+);
+-- Create the Stores table
+CREATE TABLE IF NOT EXISTS Stores (
+    Transaction_Store_ID SERIAL PRIMARY KEY,
+    SKU_ID SERIAL REFERENCES Product_Grid(SKU_ID) NOT NULL,
+    SKU_Purchase_Price NUMERIC(10, 2) NOT NULL,
+    SKU_Retail_Price NUMERIC(10, 2) NOT NULL
+);
+-- Create the Transactions table
+CREATE TABLE IF NOT EXISTS Transactions (
+    Transaction_ID SERIAL PRIMARY KEY,
+    Customer_Card_ID INT REFERENCES Cards(Customer_Card_ID) NOT NULL,
+    Transaction_Summ NUMERIC(10, 2) NOT NULL,
+    Transaction_DateTime TIMESTAMP NOT NULL,
+    Transaction_Store_ID INT REFERENCES Stores(Transaction_Store_ID) NOT NULL
+);
+-- Create the Checks table
+CREATE TABLE IF NOT EXISTS Checks (
+    Transaction_ID INT REFERENCES Transactions(Transaction_ID) PRIMARY KEY,
+    SKU_ID INT REFERENCES Product_Grid(SKU_ID) NOT NULL,
+    SKU_Amount NUMERIC(10, 2) NOT NULL,
+    SKU_Summ NUMERIC(10, 2) NOT NULL,
+    SKU_Summ_Paid NUMERIC(10, 2) NOT NULL,
+    SKU_Discount NUMERIC(10, 2) NOT NULL
+);
+-- Create the Analysis_Date table
+CREATE TABLE IF NOT EXISTS Analysis_Date (Analysis_Formation TIMESTAMP PRIMARY KEY);
