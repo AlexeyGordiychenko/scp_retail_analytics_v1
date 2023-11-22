6f1137c96d5372cdb9b0426114b6d9d11a0ed2c9
Bors Beckie <borsbeck@ex-j7.nsk.21-school.ru>
2023-11-22 13:38:33 +0700

    add direct import

--- src/part1_fill_tables.sql
+++ src/part1_fill_tables.sql
@@ -6,3 +6,12 @@ CALL import('Stores', '../datasets/Stores_Mini.tsv', E'\t');
 CALL import('Transactions', '../datasets/Transactions_Mini.tsv', E'\t');
 CALL import('Checks', '../datasets/Checks_Mini.tsv', E'\t');
 CALL import('Analysis_Date', '../datasets/Date_Of_Analysis_Formation.tsv', E'\t');
+
+\COPY Customers FROM '../datasets/Personal_Data_Mini.tsv' WITH CSV DELIMITER E'\t';
+\COPY Cards FROM '../datasets/Cards_Mini.tsv' WITH CSV DELIMITER E'\t';
+\COPY Product_Groups FROM '../datasets/Groups_SKU_Mini.tsv' WITH CSV DELIMITER E'\t';
+\COPY Products FROM '../datasets/SKU_Mini.tsv' WITH CSV DELIMITER E'\t';
+\COPY Stores FROM '../datasets/Stores_Mini.tsv' WITH CSV DELIMITER E'\t';
+\COPY Transactions FROM '../datasets/Transactions_Mini.tsv' WITH CSV DELIMITER E'\t';
+\COPY Checks FROM '../datasets/Checks_Mini.tsv' WITH CSV DELIMITER E'\t';
+\COPY Analysis_Date FROM '../datasets/Date_Of_Analysis_Formation.tsv' WITH CSV DELIMITER E'\t';
