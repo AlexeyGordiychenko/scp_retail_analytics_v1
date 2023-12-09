2fd5a7270a25f8bcc02c0f8eeca2919c2234e044
elidacon <elidacon@student.21-school.ru>
2023-12-09 21:28:27 +0500

    Refactor and optimize part2_2 (purchase history)

--- src/docker/part2_2_PurchaseHistoryView.sql
+++ src/docker/part2_2_PurchaseHistoryView.sql
@@ -1,19 +1,23 @@
-DROP VIEW IF EXISTS v_purchase_history;
 CREATE OR REPLACE VIEW v_purchase_history AS
-SELECT DISTINCT customer_id, 
-				ct.transaction_id, 
-				transaction_datetime,
-				group_id,
-				sum(sku_amount * sku_purchase_price) OVER w AS group_cost,
-				sum(sku_summ) OVER w AS group_summ,
-				sum(sku_summ_paid) OVER w AS group_summ_paid 
-FROM customer_transactions() ct
-JOIN checks ch ON ct.transaction_id = ch.transaction_id
-JOIN products p ON ch.sku_id = p.sku_id 
-JOIN stores s ON ch.sku_id = s.sku_id 
-				AND ct.transaction_store_id = s.transaction_store_id
-WINDOW w AS (PARTITION BY customer_id, ct.transaction_id, group_id)
-ORDER BY customer_id, transaction_id, group_id;
+SELECT
+    customer_id,
+    ct.transaction_id,
+    transaction_datetime,
+    group_id,
+    sum(sku_amount * sku_purchase_price) AS group_cost,
+    sum(sku_summ) AS group_summ,
+    sum(sku_summ_paid) AS group_summ_paid
+FROM
+    customer_transactions() ct
+    LEFT JOIN checks ch ON ct.transaction_id = ch.transaction_id
+    LEFT JOIN products p ON ch.sku_id = p.sku_id
+    LEFT JOIN stores s ON ct.transaction_store_id = s.transaction_store_id
+        AND ch.sku_id = s.sku_id
+GROUP BY
+	customer_id,
+	ct.transaction_id,
+	transaction_datetime,
+	group_id
+;
 
-SELECT *
-FROM v_purchase_history;
+
