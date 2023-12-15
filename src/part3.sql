6efc372fc0a7108ac5876e83251f31f323254d6f
elidacon <elidacon@student.21-school.ru>
2023-12-15 11:39:26 +0500

    Add 'LOGIN' to part3.sql (to allow login to the roles)

--- src/part3.sql
+++ src/part3.sql
@@ -1,8 +1,8 @@
 -- Create Administrator role with full access
-CREATE ROLE "Administrator" SUPERUSER;
+CREATE ROLE "Administrator" SUPERUSER LOGIN;
 
 -- Create Visitor role
-CREATE ROLE "Visitor";
+CREATE ROLE "Visitor" LOGIN;
 
 -- Allow Visitor to select from all tables
 GRANT pg_read_all_data TO "Visitor";
